47f44270968f8b0dcb3a678d0921f8c3ff9a195f
Laura <milanbartky@gmail.com>
2025-11-01 19:44:21 +0100

    install starship via pacman

--- update.sh
+++ update.sh
@@ -31,7 +31,7 @@ echo "Checking dependencies..."
 # install starship
 if ! command -v starship >/dev/null 2>&1; then
 	echo "Installing starship..."
-	curl -sS https://starship.rs/install.sh | sh
+	sudo pacman -Sy starship
 fi
 
 # install ripgrep
