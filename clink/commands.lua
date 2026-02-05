df9ddb78dbe3ce766a8216e9c7429022101351c9
Laura <laura@wiese2.org>
2026-02-05 16:10:45 +0100

    test less deep

--- clink/commands.lua
+++ clink/commands.lua
@@ -295,7 +295,7 @@ commands["test"] = function(args)
         utils.printf("[go] testing %s", utils.clean_path(target_dir))
 
         return string.format(
-            "cmd /c \"set CGO_ENABLED=1 && set CC=zig cc && set CXX=zig c++ && go test -v ./... %s\"",
+            "cmd /c \"set CGO_ENABLED=1 && set CC=zig cc && set CXX=zig c++ && go test -v . %s\"",
             extra_args
         )
 	end
