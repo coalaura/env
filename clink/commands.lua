4b3395981d97abed1c250b900d98cee1a1146224
Laura <laura@wiese2.org>
2026-02-24 21:02:21 +0100

    fix extra args

--- clink/commands.lua
+++ clink/commands.lua
@@ -295,7 +295,7 @@ commands["test"] = function(args)
         utils.printf("[go] testing %s", utils.clean_path(target_dir))
 
         return string.format(
-            "cmd /c \"set CGO_ENABLED=1 && set CC=zig cc && set CXX=zig c++ && go test -v%s .\"",
+            "cmd /c \"set CGO_ENABLED=1 && set CC=zig cc && set CXX=zig c++ && go test -v %s .\"",
             extra_args
         )
 	end
@@ -362,7 +362,7 @@ commands["run"] = function(args)
         utils.printf("[go] running %s", utils.clean_path(main_dir))
 
         return string.format(
-            "cmd /c \"set CGO_ENABLED=1 && set ^\"CC=zig cc -target x86_64-windows-gnu^\" && set ^\"CXX=zig c++ -target x86_64-windows-gnu^\" && go run%s %s\"",
+            "cmd /c \"set CGO_ENABLED=1 && set ^\"CC=zig cc -target x86_64-windows-gnu^\" && set ^\"CXX=zig c++ -target x86_64-windows-gnu^\" && go run %s %s\"",
             extra_args,
             main_dir
         )
@@ -458,7 +458,7 @@ commands["build"] = function(args)
         end
 
         return string.format(
-            "cmd /c \"set ^\"GOOS=%s^\" && set ^\"GOARCH=amd64^\" && set ^\"CGO_ENABLED=1^\"%s && go build -trimpath -buildvcs=false -ldflags ^\"%s^\"%s -o ^\"%s^\" %s\"",
+            "cmd /c \"set ^\"GOOS=%s^\" && set ^\"GOARCH=amd64^\" && set ^\"CGO_ENABLED=1^\"%s && go build -trimpath -buildvcs=false -ldflags ^\"%s^\" %s -o ^\"%s^\" %s\"",
             target_os,
             env_cc,
             ldflags,
