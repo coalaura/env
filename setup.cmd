2caaf7b854561fb7d33ce07cde4561ad483cdd53
Laura <laura@wiese2.org>
2026-03-24 02:43:10 +0100

    cleanup windows

---
+++ setup.cmd
@@ -0,0 +1,121 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+net session >nul 2>&1
+
+if not %errorlevel%==0 (
+    echo Setup requires admin privileges, relaunching...
+    
+    sudo rio -e "setup.cmd"
+    
+    goto :END
+)
+
+echo Setting up and upgrading dependencies...
+
+:: default binary folder
+if not exist "%USERPROFILE%\.bin" (
+    mkdir "%USERPROFILE%\.bin"
+)
+
+:: starship
+echo Installing/Upgrading starship...
+
+where starship >nul 2>&1
+
+if %errorlevel%==0 (
+    winget upgrade --id Starship.Starship
+) else (
+    winget install --id Starship.Starship
+)
+
+:: coreutils
+echo Installing/Upgrading coreutils...
+
+where coreutils >nul 2>&1
+
+if %errorlevel%==0 (
+    winget upgrade uutils.coreutils
+) else (
+    winget install uutils.coreutils
+)
+
+:: ripgrep
+echo Installing/Upgrading ripgrep...
+
+where rg >nul 2>&1
+
+if %errorlevel%==0 (
+    winget upgrade BurntSushi.ripgrep.MSVC
+) else (
+    winget install BurntSushi.ripgrep.MSVC
+)
+
+:: bun
+echo Installing/Upgrading bun...
+
+where bun >nul 2>&1
+
+if %errorlevel%==0 (
+    bun upgrade
+) else (
+    powershell -c "irm bun.sh/install.ps1|iex"
+)
+
+:: biome
+echo Installing/Upgrading biome...
+
+curl -Ls https://github.com/biomejs/biome/releases/latest/download/biome-win32-x64.exe -o "%USERPROFILE%\.bin\biome.exe"
+
+:: zig
+echo Installing/Upgrading zig...
+
+where zig >nul 2>&1
+
+if %errorlevel%==0 (
+    winget upgrade --id zig.zig
+) else (
+    winget install --id zig.zig
+)
+
+:: upx
+echo Installing/Upgrading upx...
+
+where upx >nul 2>&1
+
+if %errorlevel%==0 (
+    winget upgrade --id UPX.UPX
+) else (
+    winget install --id UPX.UPX
+)
+
+:: time
+echo Installing/Upgrading time...
+
+curl -L "https://github.com/coalaura/time/releases/download/v0.1.0/time_v0.1.0_windows_amd64.exe" -o "%USERPROFILE%\.bin\time.exe"
+
+:: environment upgrader
+echo Loading env upgrader...
+
+curl -fssl -o "%TEMP%\env_upgrader.exe" "https://coalaura.github.io/env/upgrader_windows.exe"
+
+if not %errorlevel%==0 (
+    echo Failed to download upgrader
+    
+	del /q "%TEMP%\env_upgrader.exe" 2>nul
+) else (
+    echo Running upgrader...
+    
+	"%TEMP%\env_upgrader.exe"
+
+    del /q "%TEMP%\env_upgrader.exe" 2>nul
+)
+
+echo Done.
+
+pause >nul
+
+:END
+
+endlocal
--- update.cmd
+++ update.cmd
@@ -2,19 +2,21 @@
 
 setlocal enabledelayedexpansion
 
+echo Updating configuration files...
+
 :: rio config
-echo Copying rio config...
+where rio >nul 2>&1
 
-if not exist "%LOCALAPPDATA%\rio" (
-    mkdir "%LOCALAPPDATA%\rio"
+if %errorlevel%==0 (
+    echo Copying rio config...
+
+    if not exist "%LOCALAPPDATA%\rio\themes" (
+        mkdir "%LOCALAPPDATA%\rio\themes"
+    )
+
+    copy /y "rio\config.toml" "%LOCALAPPDATA%\rio\config.toml" >nul
+    copy /y "rio\themes\catppuccin-macchiato.toml" "%LOCALAPPDATA%\rio\themes\catppuccin-macchiato.toml" >nul
 )
-
-if not exist "%LOCALAPPDATA%\rio\themes" (
-    mkdir "%LOCALAPPDATA%\rio\themes"
-)
-
-copy "rio\config.toml" "%LOCALAPPDATA%\rio\config.toml"
-copy "rio\themes\catppuccin-macchiato.toml" "%LOCALAPPDATA%\rio\themes\catppuccin-macchiato.toml"
 
 :: clink config
 echo Copying clink config...
@@ -23,109 +25,59 @@ if not exist "%LOCALAPPDATA%\clink" (
     mkdir "%LOCALAPPDATA%\clink"
 )
 
-copy "clink\clink_settings" "%LOCALAPPDATA%\clink\clink_settings"
-copy "clink\commands.lua" "%LOCALAPPDATA%\clink\commands.lua"
-copy "clink\init.lua" "%LOCALAPPDATA%\clink\init.lua"
-copy "clink\json.lua" "%LOCALAPPDATA%\clink\json.lua"
-copy "clink\utils.lua" "%LOCALAPPDATA%\clink\utils.lua"
+copy /y "clink\clink_settings" "%LOCALAPPDATA%\clink\clink_settings" >nul
+copy /y "clink\commands.lua" "%LOCALAPPDATA%\clink\commands.lua" >nul
+copy /y "clink\init.lua" "%LOCALAPPDATA%\clink\init.lua" >nul
+copy /y "clink\json.lua" "%LOCALAPPDATA%\clink\json.lua" >nul
+copy /y "clink\utils.lua" "%LOCALAPPDATA%\clink\utils.lua" >nul
 
 :: starship config
-echo Copying starship config...
+where starship >nul 2>&1
 
-if not exist "%USERPROFILE%\.config" (
-    mkdir "%USERPROFILE%\.config"
+if %errorlevel%==0 (
+    echo Copying starship config...
+
+    if not exist "%USERPROFILE%\.config" (
+        mkdir "%USERPROFILE%\.config"
+    )
+
+    copy /y "starship\starship.toml" "%USERPROFILE%\.config\starship.toml" >nul
 )
 
-copy "starship\starship.toml" "%USERPROFILE%\.config\starship.toml"
-
 :: git config
-echo Copying git config...
+where git >nul 2>&1
 
-copy "git\.gitconfig" "%USERPROFILE%\.config\.gitconfig_env"
+if %errorlevel%==0 (
+    echo Copying git config...
 
-:: default binary folder
-if not exist "%USERPROFILE%\.bin" (
-    mkdir "%USERPROFILE%\.bin"
+    if not exist "%USERPROFILE%\.config" (
+        mkdir "%USERPROFILE%\.config"
+    )
+
+    copy /y "git\.gitconfig" "%USERPROFILE%\.config\.gitconfig_env" >nul
 )
 
 :: biome config
-echo Copying biome config...
-
-copy "biome\biome.json" "%USERPROFILE%\biome.json"
-copy "biome\biome.json" "D:\biome.json"
-
-:: vscode keybinds.json
-echo Copying vscode keybinds...
-
-copy ".vscode\keybinds.json" "%APPDATA%\Code\User\keybindings.json"
-
-:: dependencies
-echo Checking dependencies...
-
-:: install starship
-where starship >nul 2>&1
-
-if not %errorlevel%==0 (
-    echo Installing starship...
-    winget install --id Starship.Starship
-)
-
-:: install coreutils
-where coreutils >nul 2>&1
-
-if not %errorlevel%==0 (
-    echo Installing coreutils...
-    winget install uutils.coreutils
-)
-
-:: install ripgrep
-where rg >nul 2>&1
-
-if not %errorlevel%==0 (
-    echo Installing ripgrep...
-    winget install BurntSushi.ripgrep.MSVC
-)
-
-:: install bun
-where bun >nul 2>&1
-
-if not %errorlevel%==0 (
-    echo Installing bun...
-    powershell -c "irm bun.sh/install.ps1|iex"
-)
-
-:: install biome
 where biome >nul 2>&1
 
-if not %errorlevel%==0 (
-    echo Installing biome...
-    curl -Ls https://github.com/biomejs/biome/releases/latest/download/biome-win32-x64.exe -o "%USERPROFILE%\.bin\biome.exe"
+if %errorlevel%==0 (
+    echo Copying biome config...
+
+    copy /y "biome\biome.json" "%USERPROFILE%\biome.json" >nul
+    copy /y "biome\biome.json" "D:\biome.json" >nul
 )
 
-:: install zig
-where zig >nul 2>&1
+:: vscode keybinds.json
+if exist "%APPDATA%\Code\User" (
+    echo Copying vscode keybinds...
 
-if not %errorlevel%==0 (
-    echo Installing zig...
-    winget install --id zig.zig
+    copy /y ".vscode\keybinds.json" "%APPDATA%\Code\User\keybindings.json" >nul
 )
 
-:: install upx
-where upx >nul 2>&1
-
-if not %errorlevel%==0 (
-    echo Installing upx...
-    winget install --id UPX.UPX
-)
-
-:: install time
-if not exist "%USERPROFILE%\.bin\time.exe" (
-    echo Installing time...
-    curl -L "https://github.com/coalaura/time/releases/download/v0.1.0/time_v0.1.0_windows_amd64.exe" -o "%USERPROFILE%\.bin\time.exe"
-)
-
-endlocal
+echo Done.
 
 if %0 == "%~0" (
     pause >nul
 )
+
+endlocal
